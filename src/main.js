9e5464dd6119e719c422f67d068b2268789e9368
Meghsham Kapure <meghshamvinayakkapure@gmail.com>
2026-03-25 01:10:28 +0530

    config(database): mongoose database connection 1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
-console.log('Hello World !!!');
+import "dotenv/config";
+import mongoose from "mongoose";
+import { appName, dbName } from "./constants.js";
+import express from "express";
+
+const appPort = process.env.APP_PORT ?? 5000;
+const appUrl = process.env.MONGODB_URI;
+console.log(appUrl);
+
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${dbName}`);
+    app.on("error", (error) => {
+      console.error(`ERROR ${error}`);
+      throw new Error("Server Initialization Failed!");
+    });
+
+    app.listen(appPort, () => {
+      console.log(`${appName} is started running on ${appPort}`);
+    });
+  } catch (error) {
+    console.error(`ERROR ${error}`);
+  }
+})();
