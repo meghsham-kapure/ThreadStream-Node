6f2c0089dd8649b52fe9cf55ad5b2990d88ec8fb
Meghsham Kapure <meghshamvinayakkapure@gmail.com>
2026-05-01 13:30:02 +0530

    feat(health-check): enhance health-check endpoint response with structured ApiResponse

--- src/routes/info-routes.js
+++ src/routes/info-routes.js
@@ -1,11 +1,20 @@
 import { Router } from 'express';
 import { } from "./../controllers/info-controller.js"
+import ApiResponse from '../utils/api-response.js';
 
 const infoRouter = Router();
 
 infoRouter.route("/health-check")
   .get((request, response) => {
-    res.json({ message: "hello world" });
-  })
+    response
+      .status(200)
+      .json(new ApiResponse(200, request.body, {
+        "status": "available",
+        "version": "0.0.0.1",
+        "message": "Server is up and running",
+        "database": "mongodb",
+        "cloud_storage": "cloudinary"
+      }));
+  });
 
 export default infoRouter
--- src/utils/api-response.js
+++ src/utils/api-response.js
@@ -1,7 +1,7 @@
 class ApiResponse {
-  constructor(statusCode, data, message) {
+  constructor(statusCode, data, message, metaData) {
     this.statusCode = statusCode;
-    this.data = data;
+    this.success = statusCode < 400;
     this.message = message || "Request successfully served";
     this.success = statusCode < 400;
   }
